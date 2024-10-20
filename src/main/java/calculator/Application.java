d176dad8e3e381a03b0b284d70fab34345ca651b
jungchoib <jungchoi1@naver.com>
2024-10-21 01:09:51 +0900

    feat(input): add input feature with output and invalid input exception handling

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
-
+import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        // input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("덧셈할 문자열을 입력해 주세요.\n");
+        input = scanner.nextLine();
+        // output
+        try {
+            long result = Long.parseLong(input);
+            System.out.print("결과 : " + result);
+        // Handle non-valid input
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다: " + input, e);
+        }
     }
 }
